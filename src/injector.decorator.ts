df1b296e734a447e0d17b742246624fa8407b41c
emmat-york <andrei.filimonchyk@gmail.com>
2025-06-01 12:34:24 +0200

    change version from "1.0.3" to "1.0.4"

--- src/injector.decorator.ts
+++ src/injector.decorator.ts
@@ -2,7 +2,7 @@ import { Constructor } from './injector.interface';
 
 /**
  * @description Decorator that marks a class as available to be provided and injected as a dependency.
- * Marking a class with @Service ensures that the compiler will generate
+ * Marking a class with @Injectable ensures that the compiler will generate
  * the necessary metadata to create the class's dependencies when the class is injected.
  **/
 export function Injectable(): Function {
