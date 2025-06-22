27b1eb789db1a4339f0ac80727e2da6076302d46
emmat-york <andrei.filimonchyk@gmail.com>
2025-06-22 14:45:05 +0200

    update INJECTOR_ERRORS const part 2

--- src/injector.constant.ts
+++ src/injector.constant.ts
@@ -22,13 +22,18 @@ const getTokenName = (token: ProviderToken): string => {
   return typeof token === 'function' ? token.name : token.description;
 };
 
+const getInjectorName = (name?: string): string => {
+  return name ? `Injector: ${name}` : '';
+};
+
 export const INJECTOR_ERRORS = {
-  EMPTY_PROVIDERS: (
-    name?: string,
-  ): string => `Injector created without any providers.Consider adding providers
-     to enable dependency resolution. ${name && `Injector: ${name}`}`,
-  PROVIDER_NOT_FOUND: (token: ProviderToken, name?: string): string =>
-    `Injector Error: No provider for ${getTokenName(token)}. ${name && `Injector: ${name}`}`,
-  DECORATOR_MISSING: (constructorName: string): string =>
-    `Injector Error: Cannot instantiate class ${constructorName} because it does not have a @Injectable decorator.`,
+  EMPTY_PROVIDERS: (name?: string): string => {
+    return `Injector created without any providers. Consider adding providers to enable dependency resolution. ${getInjectorName(name)}`;
+  },
+  PROVIDER_NOT_FOUND: (token: ProviderToken, name?: string): string => {
+    return `Injector Error: No provider for ${getTokenName(token)}. ${getInjectorName(name)}`;
+  },
+  DECORATOR_MISSING: (constructorName: string): string => {
+    return `Injector Error: Cannot instantiate class ${constructorName} because it does not have the @Injectable decorator.`;
+  },
 };
